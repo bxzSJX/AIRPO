d7af2493b2ceba21cdeafe74b0fb9f848b143112
bxzSJX <166011518+bxzSJX@users.noreply.github.com>
2025-12-20 00:27:13 +0200

    Add files via upload

---
+++ AIPro/models/advanced_cnn.py
@@ -0,0 +1,62 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+class AdvancedCNN(nn.Module):
+    """
+    结构：[Conv-BN-ReLU] * 2 -> MaxPool (28->14) -> [Conv-BN-ReLU] * 2 -> MaxPool (14->7) -> [Conv-BN-ReLU] * 2 -> MaxPool (7->3) -> FC
+    """
+
+    def __init__(self, num_classes=47):
+        super(AdvancedCNN, self).__init__()
+
+        # Block 1: 28x28 -> 14x14
+        self.conv1 = nn.Conv2d(1, 32, 3, padding=1)
+        self.bn1 = nn.BatchNorm2d(32)
+        self.conv2 = nn.Conv2d(32, 32, 3, padding=1)
+        self.bn2 = nn.BatchNorm2d(32)
+        self.pool = nn.MaxPool2d(kernel_size=2)
+
+        # Block 2: 14x14 -> 7x7
+        self.conv3 = nn.Conv2d(32, 64, 3, padding=1)
+        self.bn3 = nn.BatchNorm2d(64)
+        self.conv4 = nn.Conv2d(64, 64, 3, padding=1)
+        self.bn4 = nn.BatchNorm2d(64)
+
+        # Block 3: 7x7 -> 3x3
+        self.conv5 = nn.Conv2d(64, 128, 3, padding=1)
+        self.bn5 = nn.BatchNorm2d(128)
+        self.conv6 = nn.Conv2d(128, 128, 3, padding=1)
+        self.bn6 = nn.BatchNorm2d(128)
+
+        # FC Layer
+        # 展平后维度: 128通道 * 3 * 3
+        self.fc1 = nn.Linear(128 * 3 * 3, 256)
+        self.dropout = nn.Dropout(0.5)
+        self.fc2 = nn.Linear(256, num_classes)
+
+    def forward(self, x):
+        # Block 1
+        x = F.relu(self.bn1(self.conv1(x)))
+        x = F.relu(self.bn2(self.conv2(x)))
+        x = self.pool(x)
+
+        # Block 2
+        x = F.relu(self.bn3(self.conv3(x)))
+        x = F.relu(self.bn4(self.conv4(x)))
+        x = self.pool(x)
+
+        # Block 3
+        x = F.relu(self.bn5(self.conv5(x)))
+        x = F.relu(self.bn6(self.conv6(x)))
+        x = self.pool(x)
+
+        # Flatten
+        x = x.view(x.size(0), -1)
+
+        # FC
+        x = F.relu(self.fc1(x))
+        x = self.dropout(x)
+        x = self.fc2(x)
+
+        return x
